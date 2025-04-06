9e72ff79b3579ab7d34405fef90fdd9308f8d390
NeoCyber05 <bach14062005@gmail.com>
2025-04-06 17:13:07 +0700

    create readMapInFile

--- Source/main.py
+++ Source/main.py
@@ -26,3 +26,31 @@ pygame.font.init()
 my_font = pygame.font.SysFont('Contrail One', 30)    #Score lúc game run
 my_font_2 = pygame.font.SysFont('Contrail One', 100) #Your Score ( Hiện lúc end game)
 
+
+def readMapInFile(map_name: str):
+    f = open(map_name, "r")   # Mở file để đọc
+    x = f.readline().split()  # Đọc dòng đầu tiên của file, chứa kích thước bản đồ (N và M)
+    global N, M, _map
+    _map = []                    # Khởi tạo lại bản đồ
+    N, M = int(x[0]), int(x[1])  # Gán N và M từ file vào các biến toàn cục (số hàng và số cột của bản đồ)
+
+    for _ in range(N):  # Lặp qua từng hàng của bản đồ
+        line = f.readline().split()
+        _m = []
+        for j in range(M):           # Lặp qua từng cột trong mỗi hàng
+            _m.append(int(line[j]))
+        _map.append(_m)
+
+    global PacMan
+    x = f.readline().split()  # Đọc dòng cuối cùng của file chứa tọa độ của PacMan
+
+    # Tính toán lề (margin) để căn giữa bản đồ trên màn hình
+    MARGIN["TOP"] = max(0, (HEIGHT - N * SIZE_WALL) // 2)
+    MARGIN["LEFT"] = max(0, (WIDTH - M * SIZE_WALL) // 2)
+
+    # Khởi tạo đối tượng PacMan với tọa độ lấy từ file và ảnh khởi tạo đầu tiên
+    PacMan = Player(int(x[0]), int(x[1]), IMAGE_PACMAN[0])
+
+    f.close()
+
+
